4c40d6f7d9661df81008fe9fcc6a877c9f9fb550
mashigure <mashigure@live.jp>
2020-01-01 20:36:49 +0900

    add images and about mini

--- PicoPicoBox_mini/PicoPicoBox_mini.ino
+++ PicoPicoBox_mini/PicoPicoBox_mini.ino
@@ -1,5 +1,5 @@
 //------------------------------------------------------------
-// PicoPico Box
+// PicoPico Box mini
 //------------------------------------------------------------
 #include <XBOXONE.h>
 
@@ -50,7 +50,7 @@ const int note[] = {
 //------------------------------------------------------------
 void setup()
 {
-    Serial.begin(31250); // USB MIDI
+    Serial.begin(19200);
 
     if (Usb.Init() == -1)
     {
@@ -65,7 +65,6 @@ void loop()
 {
     Usb.Task();
     toneByGamepad();
-    toneByUsbMidiIn();
     delay(1);
 }
 
@@ -93,129 +92,121 @@ void toneByGamepad()
 
         if (!last_btn.down & curr_btn.down)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 60;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('D');
             is_tone_on = true;
         }
         else if (key == 60 && last_btn.down & !curr_btn.down)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.left & curr_btn.left)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 62;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('L');
             is_tone_on = true;
         }
         else if (key == 62 && last_btn.left & !curr_btn.left)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.up & curr_btn.up)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 64;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('U');
             is_tone_on = true;
         }
         else if (key == 64 && last_btn.up & !curr_btn.up)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.right & curr_btn.right)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 65;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('R');
             is_tone_on = true;
         }
         else if (key == 65 && last_btn.right & !curr_btn.right)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.x & curr_btn.x)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 67;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('X');
             is_tone_on = true;
         }
         else if (key == 67 && last_btn.x & !curr_btn.x)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.a & curr_btn.a)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 69;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('A');
             is_tone_on = true;
         }
         else if (key == 69 && last_btn.a & !curr_btn.a)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.b & curr_btn.b)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 71;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('B');
             is_tone_on = true;
         }
         else if (key == 71 && last_btn.b & !curr_btn.b)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
         if (!last_btn.y & curr_btn.y)
         {
-            if (is_tone_on) SendNoteOff(shifted_key);
             key = 72;
             shifted_key = key + shift;
             tone(SOUND_OUT_PIN, note[shifted_key]);
-            SendNoteOn(shifted_key);
+            Serial.write('Y');
             is_tone_on = true;
         }
         else if (key == 72 && last_btn.y & !curr_btn.y)
         {
             noTone(SOUND_OUT_PIN);
-            SendNoteOff(shifted_key);
+            Serial.write('N');
             is_tone_on = false;
         }
 
@@ -240,57 +231,9 @@ void toneByGamepad()
             {
                 tone(SOUND_OUT_PIN, note[shifted_key] + 2 * analog_val);
             }
-            SendPitchBend(100 * analog_val);
         }
 
         last_analog = analog_val;
-    }
-}
-
-//------------------------------------------------------------
-// play sound by USB MIDI INPUT
-//------------------------------------------------------------
-void toneByUsbMidiIn()
-{
-    static byte data[3];
-    static int  dat_no = 0;
-    static byte key    = 0;
-
-    if (0 < Serial.available())
-    {
-        data[dat_no] = Serial.read();
-
-        if ((data[dat_no]&0x80) == 0x80)
-        {
-            data[0] = data[dat_no];
-            dat_no = 0;
-        }
-
-        if ((data[0]&0x80) == 0x80)
-        {
-            dat_no++;
-        }
-
-        if (dat_no == 3)
-        {
-            /* Note ON */
-            if(((data[0]&0xf0) == 0x90) && 0 < data[2])
-            {
-                key = data[1];
-                tone(SOUND_OUT_PIN, note[key]);
-            }
-
-            /* Note OFF */
-            else if (((data[0]&0xf0) == 0x80) || (((data[0]&0xf0) == 0x90) && data[2] == 0))
-            {
-                if (data[1] == key)
-                {
-                    noTone(SOUND_OUT_PIN);
-                }
-            }
-
-            dat_no = 0;
-        }
     }
 }
 
@@ -326,40 +269,5 @@ int getShift(void)
     if (511 < Xbox.getButtonPress(R2)) shift += -12;
 
     return shift;
-}
-
-//------------------------------------------------------------
-// Note ON
-//------------------------------------------------------------
-void SendNoteOn(byte key)
-{
-    Serial.write(0x90);
-    Serial.write(key);
-    Serial.write(0x64);
-}
-
-//------------------------------------------------------------
-// Note OFF
-//------------------------------------------------------------
-void SendNoteOff(byte key)
-{
-    Serial.write(0x80);
-    Serial.write(key);
-    Serial.write(0x00);
-}
-
-//------------------------------------------------------------
-// Pitch bend
-//------------------------------------------------------------
-void SendPitchBend(int value)
-{
-    value += 8192;
-
-    const unsigned msb = ((value >> 7) & 0x7f);
-    const unsigned lsb = (value & 0x7f);
-
-    Serial.write(0xE0);
-    Serial.write(lsb);
-    Serial.write(msb);
 }
 
